75c99ac4a759d5ff73b479ab7055a0f396cc141e
2240793908@qq.com <2240793908@qq.com>
2020-08-12 11:17:35 +0800

    修改readme.md

--- src/test/java/com/dataway/cn/DatawayApplicationTests.java
+++ src/test/java/com/dataway/cn/DatawayApplicationTests.java
@@ -3,8 +3,6 @@ package com.dataway.cn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 @SpringBootTest
@@ -14,11 +12,4 @@ class DatawayApplicationTests {
     void contextLoads() {
         System.out.println(UUID.randomUUID().toString());
     }
-
-    @Test
-    void test1(){
-        List<String> languages = Arrays.asList("java","scala","python");
-        languages.forEach(System.out::println);
-    }
-
 }
--- src/test/java/com/dataway/cn/service/Love.java
+++
@@ -1,9 +0,0 @@
-package com.dataway.cn.service;
-
-/**
- * @author phil
- * @date 2020/5/25 0025 14:36
- */
-public interface Love {
-    void display(String name);
-}
